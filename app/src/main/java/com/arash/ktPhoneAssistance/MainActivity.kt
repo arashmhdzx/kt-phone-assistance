7c7b8bde9b976cc46e4ac8d87a3df292178c2a5a
arash mehdizadeh <arash.mehdiizadeh@gmail.com>
2024-11-16 22:51:41 +0330

    Initial commit

---
+++ app/src/main/java/com/arash/ktPhoneAssistance/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.arash.ktPhoneAssistance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.arash.ktPhoneAssistance.ui.theme.KtphoneassistanceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KtphoneassistanceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KtphoneassistanceTheme {
+        Greeting("Android")
+    }
+}
